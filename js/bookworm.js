5c18e993a6606acc1fc675535988ef3c2d6241fd
eio <6874617+eio@users.noreply.github.com>
2026-03-28 13:54:10 +0100

    Through the Time Barrier: A Study in Precognition and Modern Physics by Danah Zohar

--- js/bookworm.js
+++ js/bookworm.js
@@ -129,7 +129,7 @@ function addEntity( texture, name, link ) {
 };
 
 function initCamera( zoom ) {
-	zoom = zoom ? zoom : 27;
+	zoom = zoom ? zoom : 29;
 	camera.position.set( 0, 4, zoom );
 	camera.lookAt( new THREE.Vector3() );
 	if ( group ) {
